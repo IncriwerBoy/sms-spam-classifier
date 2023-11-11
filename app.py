cda141818cd3ad53edc6c608c5fba3f50f03c4db
Labhesh Mundhada <labheshmundhada23@gmail.com>
2023-11-11 15:09:04 +0530

    app update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 nltk.download('punkt')
-
+nltk.download('stopwords')
 ps = PorterStemmer()
 
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
